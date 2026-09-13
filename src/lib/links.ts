3a11e63bad71db9d0c8b4be3feb95cf5a0cfc253
Gleen Ryan <tjiagleenryan@gmail.com>
2026-09-13 20:14:15 +0800

    update App Store download link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,2 +1,2 @@
-// Replace this one URL when Omawe is available on the App Store.
-export const OMAWE_DOWNLOAD_URL = "https://testflight.apple.com/join/9b4DaTtE";
+export const OMAWE_DOWNLOAD_URL =
+  "https://apps.apple.com/id/app/omawe-group-trip-tracker/id6788641544";
