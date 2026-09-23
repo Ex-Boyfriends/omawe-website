ed92bab9cbe77bffa37229f046836da7f84da704
Gleen Ryan <tjiagleenryan@gmail.com>
2026-09-23 15:07:53 +0800

    fix: serve production AASA through website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        destination:
+          "https://api.omaweapp.my.id/.well-known/apple-app-site-association",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
